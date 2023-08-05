7bbc0cf5e494caa23de3d1bbbcbf14ea519ac8d3
lukicenturi <lukicenturi@gmail.com>
2023-08-05 17:15:18 +0700

    refactor: unify the output of the embedding and contents

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,63 +2,77 @@ require 'csv'
 require 'matrix'
 require 'numo/narray'
 require 'langchainrb'
+require 'tokenizers'
 require Rails.root.join('lib', 'openai_module')
 
 class MainController < ApplicationController
-  def vector_similarity(x, y)
+  # Calculate dot product of given two vector
+  def calc_similarity(x, y)
     x_array = Numo::DFloat.cast(x)
     y_array = Numo::DFloat.cast(y)
     x_array.dot(y_array)
   end
 
-  def load_embeddings(file_name)
-    embeddings = {}
+  # Get the data of each sections (content and embedding vector)
+  def load_sections_data(file_name)
+    data = {}
 
     CSV.foreach(file_name, headers: true) do |row|
       title = row['title']
-      vector = []
+      content = row['content']
 
+      # Unify the vector value that was spread in the numeric fields of the csv
+      embedding = []
       row.each_with_index do |(key, value), index|
-        next if key == 'title'
-        vector.push(value.to_f)
+        next if key == 'title' || key == 'content'
+        embedding.push(value.to_f)
       end
 
-      embeddings[title] = vector
+      data[title] = {
+        content: content,
+        embedding: embedding
+      }
     end
 
-    embeddings
+    data
   end
 
-  def get_most_relevant_document_sections(embeddings, question, texts)
+  def get_most_relevant_sections(sections_data, question)
     question_embedding = OpenAIModule.get_query_embedding(question)
 
-    document_similarities = []
-
-    embeddings.each_with_index do |(_, doc_embedding), index|
-      similarity = vector_similarity(question_embedding, doc_embedding)
-      document_similarities << [similarity, index]
+    # Get the relevancy of each section by comparing the embedding similarity with the question
+    section_contents_with_relevancy = []
+    sections_data.each_with_index do |(_, section), index|
+      relevancy = calc_similarity(question_embedding, section[:embedding])
+      section_contents_with_relevancy << {
+        relevancy: relevancy,
+        content: section[:content]
+      }
     end
 
-    num_top_documents = 5
-    top_document_similarities = document_similarities.take(num_top_documents)
+    # Sort all section by the relevancy with the question, higher first
+    sorted_section_by_relevancy = section_contents_with_relevancy.sort_by { |entry| -entry[:relevancy] }
 
-    selected_sections = []
-    top_document_similarities.each do |top_document|
-      index = top_document[0]
-      selected_sections.push(texts[index]['content'])
+    # Take 5 most relevant sections, and unify the content
+    num_top_sections = 5
+    top_similar_sections = sorted_section_by_relevancy.take(num_top_sections)
+    top_section_texts = top_similar_sections.map do |section|
+      section[:content]
     end
+    relevant_section_text = top_section_texts.join('\n\n')
 
-    joined_relevant_sections = selected_sections.join('\n\n')
-
-    return Langchain::Chunker::RecursiveText.new(joined_relevant_sections, chunk_size: 1000).chunks[0][:text]
+    # Only get first 500 tokens from the unified content
+    max_tokens = 500
+    tokenizer = Tokenizers::from_pretrained("gpt2")
+    top_tokens = tokenizer.encode(relevant_section_text).ids.take(max_tokens)
+    decoded_tokens = tokenizer.decode(top_tokens)
+    return decoded_tokens
   end
 
   def get_relevant_context(question)
-    csv_text_path = 'book.pdf.pages.csv'
-    csv_embedding_path = 'book.pdf.embeddings.csv'
-    texts = CSV.read(csv_text_path, headers: true).map(&:to_h)
-    embeddings = load_embeddings(csv_embedding_path)
-    relevant_sections = get_most_relevant_document_sections(embeddings, question, texts)
+    data_path = 'files/output/output.pdf.embeddings.csv'
+    sections_data = load_sections_data(data_path)
+    relevant_sections = get_most_relevant_sections(sections_data, question)
   end
 
   def construct_prompt(question)
@@ -74,7 +88,6 @@ class MainController < ApplicationController
   def ask
     question = params[:question]
     modified_question = question.end_with?('?') ? question : question + '?'
-
     prompt = construct_prompt(modified_question)
     result = OpenAIModule.get_completions_answer(prompt)
     render json: { response: result }
--- app/services/embedding_service.rb
+++ app/services/embedding_service.rb
@@ -1,40 +1,34 @@
 require 'pdf-reader'
-require 'tokenizers'
 require 'csv'
 require 'langchainrb'
 require Rails.root.join('lib', 'openai_module')
 
 class EmbeddingService
-  def self.count_tokens(text)
-    tokenizer = Tokenizers::from_pretrained("gpt2")
-    length = tokenizer.encode(text).ids.length
-    return length
-  end
-
   def self.clean_text(text)
     text.gsub(/\s+/, ' ').strip
   end
 
   def self.extract_pages(file_path)
-    texts = ''
     pages_data = []
 
+    # Get all texts from the PDF
+    texts = ''
     PDF::Reader.open(file_path) do |reader|
       reader.pages.each do |page|
         texts += page.text
       end
     end
 
-    chunked_texts = Langchain::Chunker::RecursiveText.new(texts, chunk_size: 1000, chunk_overlap: 100).chunks
+    # Use RecursiveText from Langchain to chunk the texts into semantically related pieces of text
+    # https://python.langchain.com/docs/modules/data_connection/document_transformers/text_splitters/recursive_text_splitter
+    chunked_texts = Langchain::Chunker::RecursiveText.new(texts, chunk_size: 1000, chunk_overlap: 200).chunks
 
     chunked_texts.each_with_index do |chunk, index|
       text = chunk[:text]
-      tokens = count_tokens(text)
 
       page_data = {
         title: "Section #{index + 1}",
         content: clean_text(text),
-        tokens: tokens
       }
 
       pages_data << page_data
@@ -42,47 +36,21 @@ class EmbeddingService
     return pages_data
   end
 
-  def self.write_to_csv(file_path, pages_data)
-    CSV.open(file_path, 'w') do |csv|
-      csv << ['title', 'content', 'tokens']
-
-      pages_data.each do |page_data|
-        csv << [page_data[:title], page_data[:content], page_data[:tokens]]
-      end
-    end
-  end
-
-  def self.compute_doc_embeddings(pages_data)
-    embeddings = {}
-
-    pages_data.each_with_index do |obj, idx|
-      content = obj[:content]
-
-      embeddings[idx] = OpenAIModule.get_doc_embedding(content)
-    end
-
-    embeddings
-  end
-
-  def self.generate_embedding(file_path)
-    output = 'book.pdf.pages.csv'
+  def self.generate(file_path)
     pages_data = extract_pages(file_path)
-    write_to_csv(output, pages_data)
-
-    embedding_output = 'book.pdf.embeddings.csv'
-    doc_embeddings = compute_doc_embeddings(pages_data)
+    embedding_output = 'files/output/output.pdf.embeddings.csv'
 
     CSV.open(embedding_output, 'w') do |csv|
-      csv << ["title"] + (0..4095).to_a.map(&:to_s) # Column headers
+      # Column headers (title, content, followed by numbers indicating the index of the embedding vector)
+      csv << ["title"] + ["content"] + (0..4095).to_a.map(&:to_s)
 
-      doc_embeddings.each do |i, embedding|
-        csv << ["Page #{i + 1}"] + embedding
+      pages_data.each_with_index do |page_data, idx|
+        content = [page_data[:content]]
+
+        # Get the embedding of each section's content
+        embedding = OpenAIModule.get_doc_embedding(content)
+        csv << [page_data[:title]] + [page_data[:content]] + embedding
       end
     end
-  end
-
-  def self.generate
-    pdf_path = 'book2.pdf'
-    generate_embedding(pdf_path)
   end
 end
